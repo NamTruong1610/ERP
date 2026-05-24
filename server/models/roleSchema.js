4ef9cbe81c00920f688c18bfb0b588e65f29e050
hainamtruong <HaiNam.Truong@student.uts.edu.au>
2026-05-24 18:58:21 +1000

    update: remove MongoDB legacy configs

--- server/models/roleSchema.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const roleSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true }, // e.g. 'ADMIN'
-  permissions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Permission' }]
-}, { timestamps: true })
-
-const Role = mongoose.model('Role', roleSchema);
-module.exports = Role;
